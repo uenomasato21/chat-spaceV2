19d9b9ba14de3f128503d5c8be4af0db72635ec9
uenomasato21 <masa.kendo0216@example.com>
2020-05-29 11:59:33 +0900

    Update massage.js

--- app/assets/javascripts/massage.js
+++ app/assets/javascripts/massage.js
@@ -40,35 +40,52 @@ $(function(){
       };
     }
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- var reloadMessages = function() {
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 3000);
-  }
-  var last_message_id = $('.message:last').data("message-id");
- $.ajax({
-   url: "api/messages",
-   type: "get",
-   dataType: 'json',
-   processData: false,
-   contentType: false,
-   data: {id: last_message_id}
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');   	
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: "json",
+    processData: false,
+    contentType: false
   })
-  .done(function(messages){
-    if (messages.length !== 0) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
+  .done(function(message){
+    var html =buildHTML(message);
+    $('.messages').append(html)
+    $('#new_message')[0].reset();
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+  })
+  .fail(function(){
+    alert('error')
+
+  })
+    return false;
+  })
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data("message-id");
+    var href = 'api/messages#index {:format=>"json"}' 
+    $.ajax({
+      url: href,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id }
+    })
+    .done(function (messages) {
+      var insertHTML ='';
+          messages.forEach(function(message){
+          insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
       });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
-  })
-    .fail(function() {
-      alert('error');
-     });
-    };
-  });
+    })
+    .fail(function () {    
+      alert('メッセージの取得に失敗しました');
+    });
+  };
+  }
+  setInterval(reloadMessages, 5000);
 });
